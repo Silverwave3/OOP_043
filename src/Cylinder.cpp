d738e131a9c9df93f66e964142b891824994f79d
Silverwave3 <nicolewang135@gmail.com>
2025-04-21 20:19:48 +0800

    finished Cylinder.cpp

---
+++ src/Cylinder.cpp
@@ -0,0 +1,41 @@
+# ifndef CYLINDER_CPP
+# define CYLINDER_CPP
+
+# include "Cylinder.h"
+# include <iomanip>
+# include <cmath>
+
+double Cylinder :: SurfaceArea(){
+    double ans;
+    ans = M_PI*radius*radius*2;
+    ans += M_PI*height*2*radius;
+    return ans;
+}
+
+double Cylinder :: Volume(){
+    double ans;
+    ans = M_PI*radius*radius*height;
+    return ans;
+}
+
+double Cylinder :: Circumference(){
+    double ans;
+    ans = M_PI*2*radius;
+    return ans;
+}
+
+istream & operator>>(istream & in, Cylinder & cldr)
+{
+    in >> cldr.radius >> cldr.height;
+    return in;
+}
+
+ostream & operator<<(ostream & out, Cylinder & cldr)
+{
+    out << "Circumference: " << fixed << setprecision(3) << cldr.Circumference() << endl;
+    out << "SurfaceArea: " << fixed << setprecision(3) << cldr.SurfaceArea() << endl;
+    out << "Volume: " << fixed << setprecision(3) << cldr.Volume() << endl;
+    return out;
+}
+
+# endif
